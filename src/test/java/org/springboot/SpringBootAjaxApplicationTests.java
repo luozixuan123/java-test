a2fe14bd90e1a122758b0c18ef9d2ef3508365ea
luozixuan <luozixuan@wyundata>
2017-11-28 14:10:54 +0800

    修改

--- src/test/java/org/springboot/SpringBootAjaxApplicationTests.java
+++ src/test/java/org/springboot/SpringBootAjaxApplicationTests.java
@@ -1,10 +1,14 @@
 package org.springboot;
 
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringBootTest
@@ -13,6 +17,7 @@ public class SpringBootAjaxApplicationTests {
 
 	@Test
 	public void contextLoads() {
+		Assert.assertEquals("1", "1");
 	}
 
 }
